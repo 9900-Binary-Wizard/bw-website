4e678f905b24bd18fd7d4ea4c835aed27f7011fa
Prom1s1ngYoung <yqin7516@gmail.com>
2023-09-12 20:25:20 +1000

    feat: init project
    nextjs(pages router), eslint setup, tailwind setup, directory refactor, husky setup, jest setup

---
+++ src/layouts/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import Header from "./Header";
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
+export default Layout;
